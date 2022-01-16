a89ba10bd46e617af458bf1ec36f4c7f4560151d
Eesha-Jain <eesha.jain801@gmail.com>
2022-01-15 17:54:14 -0800

    added banner

---
+++ components/banner.js
@@ -0,0 +1,40 @@
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useEffect } from 'react';
+import Link from 'next/link';
+
+export default function Banner() {
+    useEffect(() => {
+        //Local storage with banner
+        if (localStorage.getItem('banner1') == null || localStorage.getItem('timeClosed') == null) {
+            localStorage.setItem('banner1', 'block');
+            localStorage.setItem('timeClosed', 'none');
+        }
+
+        if ((Date.now() - localStorage.getItem('timeClosed')) / 1000 >= 3600) {
+            localStorage.removeItem('banner1');
+            localStorage.removeItem('timeClosed');
+        }
+
+        document.getElementById("banner1").style.display = localStorage.getItem('banner1');
+        document.getElementById("banner-x").addEventListener("click", function() {
+            localStorage.setItem('banner1', 'none');
+            document.getElementById("banner1").style.display = localStorage.getItem('banner1');
+            localStorage.setItem('timeClosed', Date.now());
+        });
+    }, []);
+
+    return (
+        <div className="Banner">
+            <div className="row banner-bar" style={{textAlign: 'center'}} id="banner1">
+                <div className="col banner-left">
+                    <p>Sign up for <a target="_blank" href="https://us02web.zoom.us/webinar/register/WN_zvOSipM-TA6bHwuBeKTVFQ">Washington STEM Advocacy Lobby Day Training</a> or <a target="_blank" href="https://www.eventbrite.com/e/washington-tech-invitational-tickets-234675168747">Washington Tech Invitationals</a></p>
+                </div>
+
+                <div className="col banner-right">
+                    <p><span className="banner-x" id="banner-x"><FontAwesomeIcon icon={faTimes} /></span></p>
+                </div>
+            </div>
+        </div>
+    );
+}
--- components/header.js
+++ components/header.js
@@ -1,10 +1,12 @@
 import Top from './top';
 import Nav from './navV1';
+import Banner from './banner';
 
 export default ({name, title, description, height}) => (
   <>
     <Top name={name} />
     <Nav name={name} />
+    <Banner />
     <div className="Header" style={{height: `${height || 50}vh`}}>
       <div className="headerborder">
         <h1>{title}</h1>
